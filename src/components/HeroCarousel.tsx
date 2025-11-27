b6fb5c27c2acd9a1ee53b9b1e14d9afe901c6fbb
Ganesh-1907 <ganesh.bora@artofliving.org>
2025-11-27 18:50:44 +0530

    responsive for hero section

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
 
-
 import img1 from "@/assets/hero/Screenshot from 2025-11-27 17-33-13.png";
 import img2 from "@/assets/hero/Screenshot from 2025-11-27 17-34-05.png";
 import img3 from "@/assets/hero/Screenshot from 2025-11-27 17-34-33.png";
 import img4 from "@/assets/hero/Screenshot from 2025-11-27 17-35-35.png";
 import img5 from "@/assets/hero/Screenshot from 2025-11-27 17-36-08.png";
-
 
 const images = [img1, img2, img3, img4, img5];
 
@@ -14,15 +12,15 @@ const HeroCarousel = () => {
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrent((prev) => (prev + 1) % images.length);
-    }, 3000);
-
+    const interval = setInterval(
+      () => setCurrent((prev) => (prev + 1) % images.length),
+      3000
+    );
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="relative w-screen h-[550px] overflow-hidden">
+    <div className="relative w-full h-56 sm:h-72 md:h-[450px] lg:h-[550px] overflow-hidden rounded-2xl">
       <img
         key={current}
         src={images[current]}
